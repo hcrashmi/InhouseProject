aff547aae06d8d7f89ec22546729f90c194e2bd7
hcrashmi <37067788+hcrashmi@users.noreply.github.com>
2018-04-27 17:12:50 +0530

    Update Abc.java

--- src/test/java/xyz/Abc.java
+++ src/test/java/xyz/Abc.java
@@ -3,7 +3,7 @@ package xyz;
 public class Abc {
 	
 	public static void main(String[] args){
-		System.out.println("Yahoo");
+		System.out.print("Yahoo");
 		System.out.println("gm");
 		System.out.println("bye");
 		System.out.println("hiii");
